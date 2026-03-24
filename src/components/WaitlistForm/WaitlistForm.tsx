0576856d11bbc76e080a7971c29d2816a3119851
Caden Boyd <caden.boyd10@gmail.com>
2026-03-24 15:01:43 -0700

    add form action to formspree

--- src/components/WaitlistForm/WaitlistForm.tsx
+++ src/components/WaitlistForm/WaitlistForm.tsx
@@ -2,14 +2,15 @@ import './WaitlistForm.css';
 
 export const WaitlistForm = () => {
     return (
-        <form className="waitlist-form" onSubmit={(e) => e.preventDefault()}>
+        <form className="waitlist-form" action="https://formspree.io/f/xbdprorq" method="POST">
             <input
                 type="email"
+                name="email"
                 placeholder="Enter your email address"
                 className="email-input"
                 required
             />
             <button type="submit" className="waitlist-btn">Join waitlist</button>
-        </form>
+        </form >
     );
 };
